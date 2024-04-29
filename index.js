4e2625abafadc6bbf0ab8643cf365d83f55e1909
bilelaa <biilelbenhlel@icloud.com>
2024-04-29 11:01:38 +0200

    checkpoint

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const express = require("express") ;
 const app = express();
 
 
-app.use(express.json());
 app.set('view engine', 'ejs');
 app.use(express.static('views'));
 
+app.use(express.json());
 
 
 const check = require("./middleware")
@@ -24,11 +24,25 @@ app.get('/', (req, res) => {
   });
 
 
-  app.get('/contact', (req, res) => {
+  app.get('/conctact', (req, res) => {
     // Render the 'index.ejs' template and pass data to it
-    res.render('contact');
+    res.render('conctact');
   });
 
+
+
+  app.post('/mail',async (req, res) => {
+    // Render the 'index.ejs' template and pass data to it
+    try {
+      const sender = require("./lib/mailer")
+      console.log(req.body)
+      await sender(req.body)
+      res.status(200).json(req.body)
+    } catch (error) {
+      console.log(error)
+      res.status(500).json(error)
+    }
+  });
 
 
 
